927b836d9fe24637dc3e9c2ded0f1cc95dc3c2fe
Wouter Vlaeyen <36659001+Vl-Wouter@users.noreply.github.com>
2019-02-06 21:16:42 +0100

    Landing Page Version 1
    - Includes menu, buttons and basic layout

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+const ToggleMenu = () => {
+  const sideMenu = document.querySelector('.side-menu');
+  sideMenu.classList.toggle('side-menu--out');
+}
+
+const openBtn = document.querySelector('#menuOpen');
+const closeBtn = document.querySelector('#menuClose');
+
+openBtn.addEventListener('click', ToggleMenu);
+closeBtn.addEventListener('click', ToggleMenu);
