4b6b01212e0b64a44ff1ec3829798326006272cf
Patrick Franz <patfra71@gmail.com>
2019-02-08 11:14:10 +0100

    Seperate the routing

--- server.js
+++ server.js
@@ -7,20 +7,9 @@ app.use('/assets', express.static('assets'));
 app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
 
-app.get('/', (req, res) => {
-  res.render('landing.ejs');
-});
-app.get('/gallery', (req, res) => {
-  res.render('gallery.ejs');
-});
-
-app.get('/calendar', (req, res) => {
-  res.render('landing.ejs');
-});
-
-app.get('/podcast', (req, res) => {
-  res.render('podcast.ejs');
-});
+// routes
+const index = require('./routes/index');
+app.use('/', index);
 
 const server = app.listen(3000, () => {
   console.log('Server running on port 3000.');
