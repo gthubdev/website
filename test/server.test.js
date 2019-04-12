0a11c8047327a7e9f3b2b1018d7fc26a12168042
Patrick Franz <patfra71@gmail.com>
2019-04-13 00:15:37 +0200

    Added linting tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,35 +1,35 @@
 const request = require('supertest');
 describe('Starting server', () => {
-  let server;
-  before(() => {
-    server = require('../server');
-  });
-  after(() => {
-    server.close();
-  });
-  describe('Routes', () => {
-    let routes200 = ['/', '/calendar', '/podcast', '/gallery'];
+	let server;
+	before(() => {
+		server = require('../server');
+	});
+	after(() => {
+		server.close();
+	});
+	describe('Routes', () => {
+		let routes200 = ['/', '/calendar', '/podcast', '/gallery'];
 
-    routes200.forEach(route => {
-      it('Should return 200 on a GET request.', (done) => {
-        describe(route, () => {
-          request(server)
-            .get(route)
-            .expect(200, done);
-        });
-      });
-    });
+		routes200.forEach(route => {
+			it('Should return 200 on a GET request.', (done) => {
+				describe(route, () => {
+					request(server)
+						.get(route)
+						.expect(200, done);
+				});
+			});
+		});
 
-    let routes404 = ['/test', '/foo', '/package.json'];
+		let routes404 = ['/test', '/foo', '/package.json'];
 
-    routes404.forEach(route => {
-      describe(route, () => {
-        it("Should return 302 on a GET request.", (done) => {
-          request(server)
-            .get(route)
-            .expect(302, done);
-        });
-      });
-    });
-  });
+		routes404.forEach(route => {
+			describe(route, () => {
+				it('Should return 302 on a GET request.', (done) => {
+					request(server)
+						.get(route)
+						.expect(302, done);
+				});
+			});
+		});
+	});
 });
