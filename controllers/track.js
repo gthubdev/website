234f86accc3cc4608202c9d971c963182ba9847e
KazuoCode <KazuoEdits@gmail.com>
2019-04-23 17:08:07 +0200

    Forgot to change a print line

--- controllers/track.js
+++ controllers/track.js
@@ -7,7 +7,7 @@ module.exports.createTrack = (req, res) => {
             util.print('Track \'' + req.body.name + '\' created');
             res.redirect('/calendar');
         }, err => {
-            util.print('Ups. Something went wrong trying to create a series.');
+            util.print('Ups. Something went wrong trying to create a track.');
             util.print(err);
             res.redirect('/calendar');
         });
