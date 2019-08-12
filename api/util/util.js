764f3e67cc43f3118c276ad991e9871ce332c52a
Patrick Franz <patfra71@gmail.com>
2019-08-12 02:27:19 +0200

    Fixed error function

--- api/util/util.js
+++ api/util/util.js
@@ -35,9 +35,8 @@ function isLoggedIn(req) {
 module.exports.isLoggedIn = isLoggedIn;
 
 // print error & redirect
-// TODO: decide what to do here
 function error(req, res, err) {
 	console.error(err);
-	res.redirect('/');
+	res.status(500).send('Unexpected fire in the car. In case of door emergencies, please call Rob.');
 }
 module.exports.error = error;
