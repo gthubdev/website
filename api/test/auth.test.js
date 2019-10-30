003d98a8981ba599d1b16d37c1def8c082b82366
Patrick Franz <patfra71@gmail.com>
2019-10-30 19:08:30 +0100

    Adjusted tests for auth to JWT-authorisation

--- api/test/auth.test.js
+++ api/test/auth.test.js
@@ -5,8 +5,9 @@ const should = require('chai').should();
 
 describe('Authentication', () => {
 	let userid, token;
+
 	// Create user testuser/admin
-	beforeEach(done => {
+	beforeEach(async () => {
 		let newuser = {
 			username: 'testadmin',
 			password: '$2a$08$PpEU2iK0atLmAkcKjXPXD.byYaw3Fxzlen3VUxB8l70U.IQkb/yZ.',
@@ -14,40 +15,36 @@ describe('Authentication', () => {
 			email: '',
 			usertype: 1
 		};
-		db.User.create(newuser)
-		.then(user => {
+
+		try {
+			const user = await db.User.create(newuser);
 			userid = user.id;
-			done();
-		}, err => {
+		} catch(err) {
 			should.not.exist(err);
-			done();
-		});
-	});
-	afterEach(done => {
-		db.User.destroy({
-			where: { id: userid }
-		}).then(response => {
-			response.should.equal(1);
-			done();
-		}, err => {
-			should.not.exist(err);
-			done();
-		});
+		}
 	});
 
-
-	it('Valid login for an admin', done => {
-		supertest(server)
-			.post('/api/auth/login')
-			.send({ username: 'testadmin', password: 'admin' })
-			.end((err, res) => {
-				res.status.should.equal(200);
-				should.not.exist(err);
-				done();
-			}, err => {
-				should.not.exist(err);
-				done();
+	afterEach(async () => {
+		try {
+			const response = await db.User.destroy({
+				where: { id: userid }
 			});
+			response.should.equal(1);
+		} catch(err) {
+			should.not.exist(err);
+		}
+	});
+
+
+	it('Valid login for an admin', async () => {
+		try {
+			await supertest(server)
+				.post('/api/auth/login')
+				.send({ username: 'testadmin', password: 'admin' })
+				.expect(200);
+		} catch(err) {
+			should.not.exist(err);
+		}
 	});
 
 	it('Invalid login', done => {
@@ -57,23 +54,23 @@ describe('Authentication', () => {
 			.expect(403, done);
 	});
 
-	it('Valid logout', done => {
-		supertest(server)
-			.post('/api/auth/login')
-			.send({ username: 'testadmin', password: 'admin' })
-			.end(async (err, res) => {
-				res.status.should.equal(200);
-				should.not.exist(err);
-				token = res.body.token;
-				supertest(server)
-					.post('/api/auth/logout')
-					.set('Authorization', 'Bearer ' + token)
-					.send()
-					.expect(200, done);
-			}, err => {
-				should.not.exist(err);
-				done();
-			});
+	it('Valid logout', async () => {
+		try {
+			const res = await supertest(server)
+				.post('/api/auth/login')
+				.send({ username: 'testadmin', password: 'admin' })
+				.expect(200);
+
+			token = res.body.token;
+
+			await supertest(server)
+				.post('/api/auth/logout')
+				.set('Authorization', 'Bearer ' + token)
+				.send()
+				.expect(200);
+		} catch(err) {
+			should.not.exist(err);
+		}
 	});
 
 	it('Invalid logout', done => {
@@ -84,31 +81,34 @@ describe('Authentication', () => {
 			.expect(401, done);
 	});
 
-	it('Changing password successfully', done => {
+	it('Changing password successfully', async () => {
 		let newpw =  {
 			username: 'testadmin',
 			oldpassword: 'admin',
 			newpassword: 'newpw'
 		};
-		supertest(server)
-			.post('/api/auth/changepassword')
-			.send(newpw)
-			.end((err, res) => {
-				res.status.should.equal(200);
-				should.not.exist(err);
-				let newpw2 = {
-					username: 'testadmin',
-					oldpassword: 'newpw',
-					newpassword: 'newerpw'
-				};
-				supertest(server)
-					.post('/api/auth/changepassword')
-					.send(newpw2)
-					.expect(200, done);
-			}, err => {
-				should.not.exist(err);
-				done();
-			});
+
+		try {
+			await supertest(server)
+				.post('/api/auth/changepassword')
+				.set('Authorization', 'Bearer ' + token)
+				.send(newpw)
+				.expect(200);
+
+			let newpw2 = {
+				username: 'testadmin',
+				oldpassword: 'newpw',
+				newpassword: 'newerpw'
+			};
+
+			await supertest(server)
+				.post('/api/auth/changepassword')
+				.set('Authorization', 'Bearer ' + token)
+				.send(newpw2)
+				.expect(200);
+		} catch(err) {
+			should.not.exist(err);
+		}
 	});
 
 	it('Changing password unsuccessfully', done => {
