32864c560019c8cf6f118be360f9f3f52ef9d816
Patrick Franz <patfra71@gmail.com>
2020-03-13 23:25:31 +0100

    Create component Create-Event.vue.

---
+++ components/resources/event/Create-Event.vue
@@ -0,0 +1,65 @@
+<template>
+<CRUDEvent
+	:show-dialog="showCRUD"
+	:active-event="event"
+	:headline="'Create an event'"
+	:action="'Create'"
+	:update-mode="false"
+	:series:="series"
+	:tracks="tracks"
+/>
+</template>
+
+<script>
+import CRUDEvent from '~/components/templates/CRUD-Event.vue';
+
+export default {
+	components: {
+		CRUDEvent
+	},
+	props: {
+		showDialog: {
+			type: Boolean, default: false
+		},
+		series: {
+			type: Array, default() { return []; }
+		},
+		tracks: {
+			type: Array, default() { return []; }
+		}
+	},
+	data: function() {
+		return {
+			showCRUD: false,
+			event: {
+				name: '',
+				track: '',
+				mainseries: '',
+				startdate: '',
+				enddate: '',
+				logo: '',
+				priority: ''
+			},
+		};
+	},
+	watch: {
+		showDialog(newValue) {
+			this.showCRUD = newValue;
+			if (newValue === true) {
+				this.event = {
+					name: '',
+					track: '',
+					mainseries: '',
+					startdate: '',
+					enddate: '',
+					logo: '',
+					priority: ''
+				};
+			}
+		}
+	}
+};
+</script>
+
+<style>
+</style>
