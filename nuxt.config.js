358f65718c9868a9808f568deea7dd8fc572f98d
Patrick Franz <patfra71@gmail.com>
2021-02-06 21:24:54 +0100

    Disable telemetry.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	// Global page headers (https://go.nuxtjs.dev/config-head)
 	head: {
-		title: 'gthubweb',
+		title: 'GTHub',
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -46,8 +46,11 @@ module.exports = {
 	build: {
 	},
 
-	//  Server Middleware
+	// Server Middleware
 	serverMiddleware: {
 		'/api': '~/api'
-	}
+	},
+
+	// Disable telemetry
+	telemetry: false
 };
