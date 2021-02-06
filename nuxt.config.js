32b6e58921ca22e27fb62886993c8aed591f9167
Patrick Franz <patfra71@gmail.com>
2021-02-06 21:44:16 +0100

    Add main SCSS file.

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,8 @@ module.exports = {
 
 	// Global CSS (https://go.nuxtjs.dev/config-css)
 	css: [
+		// main SCSS file in the project
+		'~/assets/scss/main.scss'
 	],
 
 	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
