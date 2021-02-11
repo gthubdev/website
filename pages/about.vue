008c2b0ab61da4f74b2700d0cb0956f367648d7a
Patrick Franz <patfra71@gmail.com>
2021-02-11 01:09:15 +0100

    Add pages About and Calendar.

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+<h1 class="page-headline">
+	About GTHub
+</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/calendar.vue
@@ -0,0 +1,13 @@
+<template>
+<h1 class="page-headline">
+	Calendar
+</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
