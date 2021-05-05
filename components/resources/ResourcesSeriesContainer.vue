5cf54cc9391b191941ddc22a3dcbdbc1d078677a
Patrick Franz <patfra71@gmail.com>
2021-05-05 15:50:43 +0200

    Implement search-function for series.

--- components/resources/ResourcesSeriesContainer.vue
+++ components/resources/ResourcesSeriesContainer.vue
@@ -1,35 +1,73 @@
 <template>
 <div class="resources">
-	<DataView
-		:value="series"
-		paginator-position="bottom"
-		:paginator="true"
-		:rows="10"
-		:always-show-paginator="false"
-	>
-		<template #list="slotProps">
-			<div class="w-screen flex justify-between py-2 items-center">
-				<div class="resource-list-item">
-					{{ slotProps.data.name }}
+	<div class="flex justify-between my-4">
+		<div>
+			<span class="p-input-icon-left">
+				<i class="pi pi-search" />
+				<InputText id="trackSearchTerm" v-model="searchTerm" type="text" />
+			</span>
+		</div>
+		<div>
+			<Button label="CREATE SERIES" class="p-button-raised" @click="openSeriesCrud()" />
+		</div>
+	</div>
+	<div>
+		<DataView
+			:value="displayedSeries"
+			paginator-position="bottom"
+			:paginator="true"
+			:rows="10"
+			:always-show-paginator="false"
+		>
+			<template #list="slotProps">
+				<div class="w-screen flex justify-between py-2 items-center">
+					<div class="resource-list-item">
+						{{ slotProps.data.name }}
+					</div>
 				</div>
-			</div>
-		</template>
-	</DataView>
+			</template>
+		</DataView>
+	</div>
 </div>
 </template>
 
 <script>
 import DataView from 'primevue/dataview';
+import InputText from 'primevue/inputtext';
 import { mapGetters } from 'vuex';
 
 export default {
 	components: {
-		DataView
+		DataView, InputText
+	},
+	data: function() {
+		return {
+			searchTerm: '',
+			displayedSeries: []
+		};
 	},
 	computed: {
 		...mapGetters({
 			series: 'resources/series/get'
 		})
+	},
+	watch: {
+		searchTerm(newvalue) {
+			if (newvalue.trim() === '')
+				this.displayedSeries = [...this.series];
+			else
+				this.displayedSeries = this.series.filter(t => {
+					return t.name.toLowerCase().includes(newvalue.toLowerCase());
+				});
+		}
+	},
+	created() {
+		this.displayedSeries = this.series;
+	},
+	methods: {
+		openSeriesCrud() {
+			;
+		}
 	}
 };
 </script>
