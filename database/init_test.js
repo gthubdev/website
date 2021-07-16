f568f051d008a1219fa6071ff4107c95228e4326
Patrick Franz <deltaone@debian.org>
2021-07-16 12:36:38 +0200

    Improve setup-scripts for tests.

---
+++ database/init_test.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const db = require('../api/models/');
+
+// set the delay in milliseconds
+const delay = 1000;
+
+console.log('Initializing data...');
+setTimeout(() => {
+	// eslint-disable-next-line no-path-concat
+	fs.readFile(path.join(__dirname, '/init_testdata.sql'), async (err, data) => {
+		if (err)
+			throw err;
+
+		try {
+			await db.sequelize.query(data.toString());
+			console.log('Initializing data completed.');
+			process.exit(0);
+		} catch (error) {
+			console.log(error);
+			console.log('Error initializing data.');
+			process.exit(1);
+		}
+	});
+}, delay);
---
+++ database/init_testdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO Usertype (id, name) VALUES (1, 'Admin');
+INSERT INTO Usertype (id, name) VALUES (2, 'Staff');
+
+# EventSessionType
+INSERT INTO EventSessionType (id, name) VALUES (1, 'Race');
+INSERT INTO EventSessionType (id, name) VALUES (2, 'Qualifying');
+INSERT INTO EventSessionType (id, name) VALUES (3, 'Practice');
+INSERT INTO EventSessionType (id, name) VALUES (4, 'Warm-up');
